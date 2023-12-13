7ac50117ab1524bf18494f29b199aa0715eede59
ozer09 <ozer0908@outlook.com>
2023-12-13 23:41:33 +0300

    Rutinler Sayfasi olusturuldu

--- app/src/main/java/com/ozer/productivo/ana_sayfa.java
+++ app/src/main/java/com/ozer/productivo/ana_sayfa.java
@@ -2,7 +2,10 @@ package com.ozer.productivo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ana_sayfa extends AppCompatActivity {
 
@@ -10,6 +13,30 @@ public class ana_sayfa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ana_sayfa);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.MENU_RUTINLER) {
+                startActivity(new Intent(ana_sayfa.this, rutinler.class));
+                return true;
+            } /* else if (itemId == R.id.menu_gunluk) {
+                startActivity(new Intent(MainActivity.this, GunlukActivity.class));
+                return true;
+            } else if (itemId == R.id.menu_yapilacaklar) {
+                startActivity(new Intent(MainActivity.this, YapilacaklarActivity.class));
+                return true;
+            } else if (itemId == R.id.menu_zamanlayici) {
+                startActivity(new Intent(MainActivity.this, ZamanlayiciActivity.class));
+                return true;
+            } else if (itemId == R.id.menu_muzik) {
+                startActivity(new Intent(MainActivity.this, MuzikActivity.class));
+                return true;
+            } */
+
+            return false;
+        });
+
 
     }
 }
---
+++ app/src/main/java/com/ozer/productivo/rutinler.java
@@ -0,0 +1,14 @@
+package com.ozer.productivo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rutinler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rutinler);
+    }
+}
