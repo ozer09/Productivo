09685c0fa9e1c5c04991ba34354c48aa3b75b49e
ozer09 <ozer0908@outlook.com>
2024-01-02 23:36:46 +0300

    Kullanıcı adı ve şifre artık boş girilemez

--- app/src/main/java/com/ozer/productivo/MainActivity.java
+++ app/src/main/java/com/ozer/productivo/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,15 +26,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(kayitIntent);
             }
         });
-
-        // Giriş yap butonu eklendi
         Button gecisButonu = findViewById(R.id.btngiris_yap);
 
         gecisButonu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent girisIntent = new Intent(MainActivity.this, yapilacaklar.class);
-                startActivity(girisIntent);
+                // Kullanıcı adı ve şifreyi kontrol et
+                EditText kullaniciAdiEditText = findViewById(R.id.kullaniciAdi);
+                EditText sifreEditText = findViewById(R.id.sifre);
+
+                String kullaniciAdi = kullaniciAdiEditText.getText().toString();
+                String sifre = sifreEditText.getText().toString();
+
+                if (kullaniciAdi.isEmpty() || sifre.isEmpty()) {
+                    // Eğer kullanıcı adı veya şifre boşsa hata mesajını göster
+                    Toast.makeText(MainActivity.this, "Kullanıcı adı ve şifre boş olamaz", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Boş değilse yapılacak işlemleri buraya ekle
+                    Intent girisIntent = new Intent(MainActivity.this, yapilacaklar.class);
+                    startActivity(girisIntent);
+                }
             }
         });
     }
