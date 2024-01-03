d37119861b4b272546201bc0f4aa42733d89a7bc
ozer09 <ozer0908@outlook.com>
2024-01-04 00:58:07 +0300

    Görev ekle sil butonları eklendi sayfaları oluşturuldu style dosyası eklendi

---
+++ app/src/main/java/com/ozer/productivo/gorev_ekle.java
@@ -0,0 +1,15 @@
+package com.ozer.productivo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gorev_ekle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gorev_ekle);
+
+    }
+}
--- app/src/main/java/com/ozer/productivo/yapilacaklar.java
+++ app/src/main/java/com/ozer/productivo/yapilacaklar.java
@@ -2,7 +2,10 @@ package com.ozer.productivo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class yapilacaklar extends AppCompatActivity {
 
@@ -10,6 +13,23 @@ public class yapilacaklar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yapilacaklar);
+        Button gorevEkleButton = findViewById(R.id.gorev_ekle);
+        Button gorevSilButton =findViewById(R.id.gorev_sil);
+
+        gorevEkleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gorevEkleIntent = new Intent(yapilacaklar.this, gorev_ekle.class);
+                startActivity(gorevEkleIntent);
+            }
+        });
+        gorevSilButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gorevSilIntent = new Intent(yapilacaklar.this,gorev_sil.class);
+            }
+        });
+
 
     }
 }
